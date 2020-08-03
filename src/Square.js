4781181945c363740d06d0a70dbcc62ad7fa33e9
spagbol <38818825+pozorfluo@users.noreply.github.com>
2020-08-03 16:49:14 +0200

    Explore hooks

--- src/Square.js
+++ src/Square.js
@@ -1,26 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function formatName(user) {
   return user.firstname + ' ' + user.lastname;
 }
 
+function reverseStr(str) {
+  return str.split('').reverse().join('');
+}
+
 export const Square = (props) => {
   let greetings = 'stranger';
+
   if (props.user) {
     greetings = formatName(props.user);
   }
-  let escape_me = "javascript:/*--></title></style></textarea></script></xmp><svg/onload='+/"/+/onmouseover=1/+/[*/[]/+alert('SadPanda')//'>";
+
   return (
-    <div>
-      <h1>Howdy, {greetings} ?</h1>
-      <h2>whatchu doin round these parts ?</h2>
-      {escape_me}
-    </div>
+    <React.Fragment>
+      <h1>Howdy, {props.toggle ? greetings : reverseStr(greetings)} ?</h1>
+    </React.Fragment>
   );
 };
-// export function SquareFn(props) {
-//   const label = props.value + 'hello';
-//   return <button>{label}</button>;
-// }
+
+export const SquareFn = (props) => {
+  const [count, setCount] = useState(0);
+  const [toggle, setToggle] = useState(1);
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    function tick() {
+        setTime(new Date());
+    }
+
+    const timer_id = setInterval(tick, 1000);
+    document.title = `${count} times`;
+
+    return () => {
+        clearInterval(timer_id);
+    }
+  });
+
+  return (
+    <React.Fragment>
+      <Square user={props.user} reverse={toggle} />
+      <button onClick={() => setCount(count + 1)}>
+        {props.user.firstname}
+      </button>
+      <button onClick={() => setToggle(toggle ^ 1)}>
+        {toggle ? props.user.firstname : reverseStr(props.user.firstname)}
+      </button>
+      <h2 style={{color: toggle ? 'hotpink' : 'dodgerblue'}} onClick={() => setTime(new Date())}>
+        {time.toLocaleTimeString()}
+      </h2>
+
+    </React.Fragment>
+  );
+};
 
 // export { Square, SquareFn };
