0319245ed14d43fd29cf9f60e9714863a1473138
spagbol <38818825+pozorfluo@users.noreply.github.com>
2020-08-05 13:22:19 +0200

    Implement toy keyed todo list

--- src/Count.js
+++ src/Count.js
@@ -2,32 +2,46 @@ import React, { useState, useEffect } from 'react';
 import { Greeting } from './Greeting';
 
 export const Count = () => {
+  const [id, setId] = useState(0);
   const [count, setCount] = useState(0);
   const [list, setList] = useState([]);
-  // useEffect(() => {
-  //     console.log(props);
-  //     // setList((list) => [...list, props.count]);
-  // });
-  //   useEffect(() => {
-  //     for (let i = 0; i < 10; i++) {
-  //       setCount(i);
-  //       console.log(i);
-  //     }
-  //   }, []);
 
   return (
     <div>
       <p>{count}</p>
       <button
         onClick={() => {
+          setList([...list, [id, count]]);
           setCount(count + 1);
-          setList([...list, count]);
-        }}
+          setId(id + 1);
+        }
+      }
       >
         greet me
       </button>
-
-      <Greeting list={list} />
+      <form
+        onSubmit={(event) => {
+          const entry = event.target.new_entry;
+          if (entry.value !== null && entry.value !== '') {
+            setCount(count + 1);
+            setId(id + 1);
+            setList([...list, [id, entry.value]]);
+            entry.value = '';
+          }
+          event.preventDefault();
+        }}
+      >
+        <input type="text" name="new_entry" />
+      </form>
+      <Greeting
+        list={list}
+        onDelete={(i) => {
+          setCount(count - 1);
+          const new_list = [...list];
+          new_list.splice(i, 1);
+          setList(new_list);
+        }}
+      />
     </div>
   );
 };
--- src/Greeting.js
+++ src/Greeting.js
@@ -2,17 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const Greeting = (props) => {
-    const formatted_list = props.list.map((entry, i) => (
-        <li key={entry + i}>
-            {entry}
-        </li>
-    ));
-
+  const formatted_list = props.list.map((entry, i) => {
     return (
-        <ul style={{ listStyleType :'none'}}>{formatted_list}</ul>
+      <li key={entry[0]}>
+        {entry[0]} : {entry[1]}
+        <button onClick={() => props.onDelete(i)}>X</button>
+      </li>
     );
+  });
+
+  return <ul style={{ listStyleType: 'none' }}>{formatted_list.reverse()}</ul>;
 };
 
 Greeting.propTypes = {
-    list : PropTypes.array.isRequired,
-}
+  list: PropTypes.array.isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
