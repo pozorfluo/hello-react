e55842a08c2071ed73bd3aae782dfdcc9f73559b
spagbol <38818825+pozorfluo@users.noreply.github.com>
2020-08-06 17:03:01 +0200

    Add browsable build

---
+++ build/precache-manifest.fff3a6ca7dd94c6fd5aaa3ab24db82e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a40e6c363aa219d8ec2ddccd9a0d86b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2290d5e017f361ef97ac",
+    "url": "/static/css/main.8fa63643.chunk.css"
+  },
+  {
+    "revision": "35f9402aac0312c13fed",
+    "url": "/static/js/2.4210dace.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4210dace.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2290d5e017f361ef97ac",
+    "url": "/static/js/main.483c338d.chunk.js"
+  },
+  {
+    "revision": "87e8f3e2a47610b86ffb",
+    "url": "/static/js/runtime-main.49a1c0af.js"
+  }
+]);
